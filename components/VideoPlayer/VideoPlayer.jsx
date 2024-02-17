3852743e4f12caaee56ade4f9fe6587afd77386b
syivan <ivansyrups@gmail.com>
2024-02-17 02:42:55 -0800

    fixed bug unable to render css for embedded youtube player

--- components/VideoPlayer/VideoPlayer.jsx
+++ components/VideoPlayer/VideoPlayer.jsx
@@ -2,10 +2,7 @@
 
 import React, { useEffect } from "react";
 import { useState } from "react";
-import ReactPlayer from "react-player/lazy";
-import styles from "./VideoPlayer.module.css";
 import LiteYoutubeEmbed from "react-lite-youtube-embed";
-import "react-lite-youtube-embed/dist/LiteYoutubeEmbed.css";
 
 const VideoPlayer = ({ videoID }) => {
   const [isClient, setIsClient] = useState(false);

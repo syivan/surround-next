4e381246de4502fecc994a12f3e812a4b8c15358
syivan <ivansyrups@gmail.com>
2024-04-01 23:53:29 -0700

    api keys to .env

--- db.js
+++ db.js
@@ -10,7 +10,6 @@
 //     }
 // });
 
-
 // export default async function executeSchema({ query, values }) {
 //     try {
 //         const results = await db.query(query, values);
@@ -21,24 +20,25 @@
 //     }
 // }
 
-import mysql from "serverless-mysql"
+import mysql from "serverless-mysql";
 
+//reminder: please use .env for api_keys
 const db = mysql({
-    config: {
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        database: process.env.MYSQL_DATABASE,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD
-    }
-})
+  config: {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+  },
+});
 
 export default async function executeSchema({ query, values }) {
-    try {
-        const results = await db.query(query, values);
-        await db.end();
-        return results;
-    } catch (error) {
-        return { error };
-    }
+  try {
+    const results = await db.query(query, values);
+    await db.end();
+    return results;
+  } catch (error) {
+    return { error };
+  }
 }

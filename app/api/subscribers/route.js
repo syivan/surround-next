6d67affe19b200bc693fde16082ee1f6bbf89d61
syivan <ivansyrups@gmail.com>
2024-05-03 13:34:00 -0700

    removed redundant comments

--- app/api/subscribers/route.js
+++ app/api/subscribers/route.js
@@ -1,19 +1,3 @@
-// import executeSchema from "@/db";
-
-// export async function POST(req) {
-//     try {
-//         const test = await req.json();
-//         const result = await executeSchema({
-//             query: 'INSERT INTO users(name) VALUES(?)',
-//             values: test.name,
-//         })
-//         console.log(result)
-//         return new Response('OK');
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
 import executeSchema from "@/utils/db";
 
 export async function POST(req) {
